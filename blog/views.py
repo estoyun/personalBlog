48a1d360742f27a82a88fc4fcf7262bd282712f6
estoyun <audvuddbs@gmail.com>
2019-02-10 19:45:51 +0900

    Class Lion until 3.3

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
+
+def home(request):
+    blogs = Blog.objects # 쿼리셋
+    return render(request, 'home.html', {'blogs': blogs})
